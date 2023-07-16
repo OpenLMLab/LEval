b5df024db8202995cc472b8b858e70c8324456e4
summmeer <hisansas@gmail.com>
2023-07-17 01:26:04 +0800

    update test res for retrieve(bm25) and xgen-7b

--- Tools/turbo4k-retrieve-test.py
+++ Tools/turbo4k-retrieve-test.py
@@ -11,6 +11,8 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
+from langchain.retrievers import ElasticSearchBM25Retriever
+# you need to start the elasticsearch server at http://localhost:9200 first
 
 import json
 from tqdm import tqdm
@@ -51,20 +53,27 @@ def main():
             if args.gsm:
                 document = "\nQuestion: ".join(instructions[0].split("\nQuestion: ")[:-1])
                 instructions = ["Question: " + instructions[0].split("\nQuestion: ")[-1]]
-            with open(f"./tmp/tmp_doc_{os.path.split(file_name)[-1].split('.')[0]}_{i}.txt", 'w', encoding='utf-8') as f_tmp:
-                f_tmp.write(document)
+            tmp_doc_name = f"./tmp/tmp_doc_{os.path.split(file_name)[-1].split('.')[0]}_{i}.txt"
+            if not os.path.exists(tmp_doc_name):
+                with open(tmp_doc_name, 'w', encoding='utf-8') as f_tmp:
+                    f_tmp.write(document)
 
-            loader = TextLoader(f"./tmp/tmp_doc_{os.path.split(file_name)[-1].split('.')[0]}_{i}.txt", encoding='utf-8')
+            loader = TextLoader(tmp_doc_name, encoding='utf-8')
             documents = loader.load()
             # import pdb; pdb.set_trace()
             text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=1024, chunk_overlap=0, separator=' ')
             texts = text_splitter.split_documents(documents)
 
-            embeddings = OpenAIEmbeddings(openai_api_key=openai_key)
-            docsearch = Chroma.from_documents(texts, embeddings)
+            if args.retriever == 'BM25':
+                elasticsearch_url = "http://localhost:9200"
+                retriever = ElasticSearchBM25Retriever.create(elasticsearch_url, f"langchain-index4-doc_{os.path.split(file_name)[-1].split('.')[0]}_{i}")
+                retriever.add_texts([text.page_content for text in texts])
+            else:
+                embeddings = OpenAIEmbeddings(openai_api_key=openai_key)
+                docsearch = Chroma.from_documents(texts, embeddings)
+                retriever = docsearch.as_retriever()
 
-            qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_key, model_name="gpt-3.5-turbo-0613"), chain_type="stuff", retriever=docsearch.as_retriever(), return_source_documents=True)
-
+            qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_key, model_name="gpt-3.5-turbo-0613"), chain_type="stuff", retriever=retriever, return_source_documents=True)
             for inst, out in zip(instructions, outputs):
                 if args.gsm:
                     message = f" Make sure the response is end with The answer is _ ."
@@ -107,6 +116,7 @@ if __name__ == "__main__":
     parser.add_argument('--metric', choices=["llm_turbo_eval","llm_gpt4_eval","exam_eval", "ngram_eval"], default="auto_eval", help='metric name from "turbo_eval","gpt4_eval","auto_eval"')
     parser.add_argument('--task_path', type=str, default=None, help= 'set this if you want test a specific task , example: LEval-data/Closed-ended-tasks/coursera.jsonl or LEval-data/Closed-ended-tasks/ ')
     parser.add_argument('--task_name', type=str, default=None, help='set this if you want test a specific task from huggingface, example: coursera')
+    parser.add_argument('--retriever', choices=["AdaEmbedding", "BM25"], default="BM25", help='choose retriever from "AdaEmbedding", "BM25"')
     parser.add_argument('--gsm', action='store_true', help='set this if you want to test gsm100 dataset')
     args = parser.parse_args()
     key_data_pairs = {}
@@ -116,7 +126,10 @@ if __name__ == "__main__":
     elif args.task_name in datasets_open_ended:
         args.metric = "ngram_eval"
 
-    openai_model = "retrieval-turbo-0613"
+    if args.retriever == "BM25":
+        openai_model = "retrieval-bm25-turbo-0613"
+    else:
+        openai_model = "retrieval-turbo-0613"
     data_save_path = f"Predictions/{args.metric}/{openai_model}"
     build_key_data_pairs(args, key_data_pairs, data_save_path)
     sys.exit(main())
